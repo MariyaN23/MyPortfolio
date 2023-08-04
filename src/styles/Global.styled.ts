2818be216ce481b66b649dd843b5752ca46aba1a
MariyaN23 <sellaite505@gmail.com>
2023-08-04 21:04:40 +0300

    Footer

--- src/styles/Global.styled.ts
+++ src/styles/Global.styled.ts
@@ -20,6 +20,8 @@ export const GlobalStyle = createGlobalStyle`
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     line-height: 1.2;
+    
+    min-width: 360px;
   }
 
   a {

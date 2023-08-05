077dfce53b04b686a5c23233d8508ab32a9742ae
MariyaN23 <sellaite505@gmail.com>
2023-08-05 18:57:33 +0300

    Refactoring Slogan, Footer

--- src/layout/sections/slogan/Slogan.tsx
+++ src/layout/sections/slogan/Slogan.tsx
@@ -5,7 +5,7 @@ import {Button} from "../../../components/Button";
 import {Container} from "../../../components/Container";
 import {FlexWrapper} from "../../../components/FlexWrapper";
 
-export const Slogan = () => {
+export const Slogan: React.FC = () => {
     return (
         <StyledSlogan>
             <Container>
@@ -20,5 +20,4 @@ export const Slogan = () => {
 
 const StyledSlogan = styled.section`
     min-height: 30vh;
-  background-color: rgba(148,236,255,0.42);
 `

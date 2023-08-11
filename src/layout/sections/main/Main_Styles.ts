724890563730b2adae90aadca548c1e829adc896
MariyaN23 <sellaite505@gmail.com>
2023-08-11 21:18:11 +0300

    Main и Slogan (Particles)

--- src/layout/sections/main/Main_Styles.ts
+++ src/layout/sections/main/Main_Styles.ts
@@ -50,7 +50,7 @@ const Photo = styled.img`
 const MainTitle = styled.h1`
 /*  font-size: 27px;
   font-weight: 400;*/
-  ${font({weight: 400, Fmax: 27, Fmin: 20})},
+  ${font({weight: 400, Fmax: 27, Fmin: 20})}
   
   p {
     display: none;
